3b3ac64b385795300f8bcb4440d434cffa325892
Ezequiel Silva <escosou@gmail.com>
2022-11-17 13:39:26 -0300

    final

--- src/Componentes/home/Home.jsx
+++ src/Componentes/home/Home.jsx
@@ -1,13 +1,14 @@
 import Leaflet from "../leaflet/Leaflet";
 import Recharts from "../recharts/Recharts";
+import './Home.css';
 
-function Home (){
-    return(
+function Home() {
+    return (
         <>
-        <h1>App Graphic</h1>
-        <Recharts/>
-        <h1>Fortaleza</h1>
-        <Leaflet/>
+            <h1 className="title">App Graphic</h1>
+            <Recharts />
+            <h1>Fortaleza</h1>
+            <Leaflet />
         </>
     );
 }
