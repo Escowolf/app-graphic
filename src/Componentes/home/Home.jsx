4ce021942fa9f9d524259f367e867980d609202e
Ezequiel Silva <escosou@gmail.com>
2022-11-17 11:43:06 -0300

    first commit

---
+++ src/Componentes/home/Home.jsx
@@ -0,0 +1,14 @@
+import Leaflet from "../leaflet/Leaflet";
+import Recharts from "../recharts/Recharts";
+
+function Home (){
+    return(
+        <>
+        <h1>App Graphic</h1>
+        <Leaflet/>
+        <Recharts/>
+        </>
+    );
+}
+
+export default Home;
---
+++ src/Componentes/recharts/Recharts.jsx
@@ -0,0 +1,38 @@
+import {
+  LineChart, ResponsiveContainer, Legend, Tooltip, Line, XAxis, YAxis,
+  CartesianGrid } from 'recharts';
+import './Recharts.css';
+
+// Sample chart data
+const pdata = [
+  { hora: '10h', ocorrencia: 11},
+  { hora: '11h', ocorrencia: 15},
+  { hora: '12h', ocorrencia: 5},
+  { hora: '13h', ocorrencia: 10},
+  { hora: '14h', ocorrencia: 9},
+  { hora: '15h', ocorrencia: 10},
+];
+
+function Recharts() {
+  return (
+    <>
+      <h1 className="graphic_title">
+        Gráfico de linhas
+      </h1>
+      <ResponsiveContainer className={"graphic_container"} aspect={3}>
+        <LineChart data={pdata} className={"graphic_area"}>
+          <CartesianGrid />
+          <XAxis dataKey="hora"
+            interval={'preserveStartEnd'} />
+          <YAxis></YAxis>
+          <Legend />
+          <Tooltip />
+          <Line dataKey="ocorrencia"
+            stroke="black" activeDot={{ r: 8 }} />
+        </LineChart>
+      </ResponsiveContainer>
+    </>
+  );
+}
+
+export default Recharts;
